7f0fbbccdab05dde01813fdf99a580fc76eb820c
David <uo288761@uniovi.es>
2025-03-16 14:47:27 +0100

    Changed instanceReader: uses correctly instances, preparedJobs and TOUPrices

--- PythonVersion/instanceReader.py
+++ PythonVersion/instanceReader.py
@@ -1,15 +1,20 @@
 from problem import Problem
+import ast
 
 class InstanceReader:
 
-    def __init__(self, pathIntances, pathPreparedJobs, pathDeadlines, pathTOUPrices):
+    def __init__(self, pathIntances, pathPreparedJobs, pathTOUPrices):
         self.problem = Problem([], 0, 0, 0, [], [], [], [], 0) # Initialize the problem
         self.pathIntances = pathIntances
         self.pathPreparedJobs = pathPreparedJobs
-        self.pathDeadlines = pathDeadlines
         self.pathTOUPrices = pathTOUPrices
         
     # Read the instance file and return the data
+    # DATA RETRIEVED:
+    # nJobs
+    # nMachines
+    # nTasks
+    # jobTasks
     def readInstance(self):
         data = []
         f = open(self.pathIntances, 'r')
@@ -18,6 +23,25 @@ class InstanceReader:
         for line in lines:
             data.append(line.split(" ")) # Get the values separated
         return data
+    
+    # Read the prepared jobs file and return the data
+    # DATA RETRIEVED:
+    # dueDates
+    # tasksMachines
+    def readPreparedJobs(self):
+        f = open(self.pathPreparedJobs, 'r')
+        data = ast.literal_eval(f.readline().strip()) # Read the array and automatically convert it 
+        f.close()
+        return data
+    
+    # Read the TOU prices file and return the data
+    # DATA RETRIEVED:
+    # energyPrices
+    def readTOUPrices(self):
+        f = open(self.pathTOUPrices, 'r')
+        lines = [line.strip() for line in f.readlines()] # Read all lines and remove the \n
+        f.close()
+        return lines
     
     def transformInstanceData(self, data):
         self.problem.nJobs = int(data[0][0])
@@ -31,6 +55,8 @@ class InstanceReader:
             tasksInJob = int(data[i+1][0])
             self.problem.nTasks += tasksInJob # Sum the number of tasks for job i
             self.problem.jobTasks.append(tasksInJob) # Append the number of tasks for job i
+            
+            """
             # j starts at 1 because the first value is the number of tasks for job i
             j = 0
             while(j < len(data[i+1])-1):
@@ -55,17 +81,67 @@ class InstanceReader:
                 
         for i in range(self.problem.nMachines):
             print(self.problem.tasksMachines[i])
-                    
-                
+                    """
+              
+    def transformPreparedJobsData(self, data):
+        for job in data:
+            self.problem.dueDates.append(job[0][0][4]) # Append the due date for the job
+            for task in job:
+                setPossibleMachines = []
+                for machine in task:
+                        machineId = machine[0]
+                        if machineId not in setPossibleMachines: # There are multiple options in the input file (just take first one)
+                            duration = machine[1]
+                            energy = machine[2]
+                            param = [duration, energy]
+                            self.problem.tasksMachines[machineId].append(param)
+                            setPossibleMachines.append(machineId)
+                # Fill the rest of the machines with -1
+                if len(setPossibleMachines) < self.problem.nMachines:
+                    for k in range(self.problem.nMachines):
+                        if k not in setPossibleMachines:
+                            self.problem.tasksMachines[k].append([-1, -1])
+                            
+        for i in range(self.problem.nMachines):
+            print(self.problem.tasksMachines[i])
+        print()
+        print(self.problem.dueDates)
+        
+    def transformTOUPricesData(self, data):
+        actualData = data[196:220] # Fixed values from file format (TOU prices from hour 0 till 24)
+        fixedData = []
+        for value in actualData:
+            fixedData.append(value.strip(','))
+        finalData = []
+        for i in range(len(fixedData)):
+            finalData.append(int(fixedData[i])/100.0)
+        self.problem.energyPrices = finalData
+
+        
             
         
         
         
         
     
-print("Test")
-instanceReader = InstanceReader(r"instances_new\instances_new\flexible_jobshop_59_10jobs_10machines_flex_high_squared.data", "", "", "")
+print("TEST")
+instanceReader = InstanceReader(r"instances_new\instances_new\flexible_jobshop_7_2jobs_3machines_flex_high_squared.data", r"preparedjobs_new\preparedjobs_new\flexible_jobshop_7_2jobs_3machines_flex_high_squared_JOBS.data", r"TOU prices\TOU prices\TOU_prices_v1")
+
+# Read and assign data retrieved to the problem
+### DATA RETRIEVED ###
+# nJobs
+# nMachines
+# nTasks
+# jobTasks
+# tasksMachines (durations only)
 dataInstance = instanceReader.readInstance()
+dataPreparedJobs = instanceReader.readPreparedJobs()
+dataTOUPrices = instanceReader.readTOUPrices()
 instanceReader.transformInstanceData(dataInstance)
+instanceReader.transformPreparedJobsData(dataPreparedJobs)
+instanceReader.transformTOUPricesData(dataTOUPrices)
+
+
+
         
     
