a3e76fb977cd5af0140338592339a9ae226fff60
David <uo288761@uniovi.es>
2025-03-25 18:47:19 +0100

    Read all individuals stored in "results" folder

--- PythonVersion/readPickle.py
+++ PythonVersion/readPickle.py
@@ -4,11 +4,14 @@ import glob # Get the general files (*)
 
 # Unpickle individuals
 individuals = []
+i = 1
 
 # Stores the individuals in the folder "results"
 path = glob.glob(os.path.join("results", "*.pkl"))
 for file in path:
     with open(file, 'rb') as pickled:
         bestInd = pickle.load(pickled)
+        print("Individual", i)
         print(bestInd)
+        i += 1
         individuals.append(bestInd)
