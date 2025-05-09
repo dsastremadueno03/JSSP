880021b8a887fa6bc36828544a243d91c076eb35
David <davidsastremadu@gmail.com>
2025-05-09 20:01:50 +0200

    CSV generation class created and data organization stated

---
+++ PythonVersion/generateCSV.py
@@ -0,0 +1,30 @@
+import ast # Automatic conversion of data
+import glob # For the path of the instance file
+
+# What data is needed to generate the CSV file?
+
+# GLOBAL DATA:
+# - nJobs
+# - nMachines
+# - nTasks
+# - Number of iterations per instance
+
+    # INDIVIDUAL PARAMETERS:
+    # - Number of individuals
+    # - Threshold
+    # - Mutation probability
+    # - Crossover probability
+
+        # TYPES:
+        # - Type of crossover
+        # - Type of mutation
+        # - Min/Max
+        # - Tardiness/Energy
+
+            # BEST RESULT OF ITERATION
+            # - Number of generations
+            # - Tardiness
+            # - Energy
+            # - Execution time
+            # - Permutations for scheduling generation???????
+
